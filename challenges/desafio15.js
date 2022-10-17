4a5269a2637eb13d259ba06f660e3985ccda362b
BiaKast <biahdrew94@gmail.com>
2022-10-17 19:04:12 -0300

    feat: query que cria o campo ultimaModificacao

--- challenges/desafio15.js
+++ challenges/desafio15.js
@@ -2,9 +2,9 @@ db.produtos.updateMany({}, { $set: { avaliacao: NumberInt(0) } });
 
 db.produtos.updateMany(
   { tags: { $all: ["bovino"] } },
-  { $set: { avaliacao: 5 } },
+  { $inc: { avaliacao: 5 } },
 );
 
-db.produtos.updateMany({ tags: { $all: ["ave"] } }, { $set: { avaliacao: 3 } });
+db.produtos.updateMany({ tags: { $all: ["ave"] } }, { $inc: { avaliacao: 3 } });
 
 db.produtos.find({}, { _id: 0, nome: 1, avaliacao: 1 });
