13e5c6201adc20df25ecff0675c9e66f91204d8d
BiaKast <biahdrew94@gmail.com>
2022-10-17 17:18:50 -0300

    refactor: mudando a escrita do desafio 9 que mexe na key valores nutricionais

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,9 +1,8 @@
 db.produtos.find(
   {
-    $and: [
-      { "valoresNutricionais.0.tipo": "calorias" },
-      { "valoresNutricionais.0.quantidade": { $lt: 500 } },
-    ],
+    valoresNutricionais: {
+      $elemMatch: { tipo: "calorias", quantidade: { $lt: 500 } },
+    },
   },
   { _id: 0, nome: 1 },
 );
